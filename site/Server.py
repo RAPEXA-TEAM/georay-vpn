d86a1c6d13748cec17c4d8654852cd6e1f921d13
rapexa <rapexamrs@gmail.com>
2022-12-27 00:47:50 +0330

    update payment for solving double spending and create user for someone who paid with crypto

--- site/Server.py
+++ site/Server.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env
+
 from flask import Flask, jsonify, request, render_template, redirect
 from flask_cors import CORS
 import Mysql
 import csv
 import hashlib
 import requests
+import names
+import random
 import CONFIG     #SERVER CONGIG
 
 app = Flask(__name__)
@@ -15,21 +18,35 @@ app.config.update(
     SECRET_KEY = CONFIG.SECRET_KEY
 )
 
-@app.route("/checkpayment",methods=["GET","POST"])
+@app.route("/payment",methods=["GET","POST"])
 def handle_check_payment():
     if request.method == "POST":
         
         txid = request.json["txid"]
-        
-        if Check_Payment(txid):
-            ret = ret = {"code" : 200, "data" : f"txid ({txid}) is valid"}
-            return jsonify(ret)
+        days = request.json["days"]
+
+        if Check_Payment(txid,days):
+            username , password = names.get_full_name().split()
+            phone = random.randint(0, 9999999999)
+            email = names.get_full_name().split()[0] + "@gmail.com"
+            answer = names.get_full_name().split()[1]
+            token = hashlib.sha256(f"{username}-{password}-{phone}-{email}-{answer}-georay".encode('utf-8')).hexdigest()
+
+            if Mysql.write_user_to_database(username, password, phone, email, str((int(days)+7)), token):
+
+                ret = {"code" : 200, "data" : {"username" : username , "token" : token}}
+                return jsonify(ret)
+
+            else:
+                ret = {"code" : 501, "data" : "'مشکل اتصال به دیتابیس'"}
+                return jsonify(ret)
         
         else:
             ret = {"code" : 404, "data" : f"txid ({txid}) is not valid"}
             return jsonify(ret)
         
-    ret = {"code" : 500, "data" : "Request not valid"}
+    prices = {"1month" : CONFIG.PRICE_ONE_MONTH, "2month" : CONFIG.PRICE_TWO_MONTH, "3month" : CONFIG.PRICE_TRE_MONTH}
+    ret = {"code" : 200, "prices" : prices}
     return jsonify(ret)
 
 @app.route('/d08ec689aef988a788aa6b5f6ed04a0efe57ca919d7d9d863d6322edd47f2d81',methods=["GET", "POST"])
@@ -139,29 +156,42 @@ def Check_User(token):
     else:
         return True
 
-def Check_Payment(txid):
+def Check_Payment(txid,days):
     '''This function check txid is valid or not'''
-    
-    apikey = CONFIG.APIKEY
-    baseurl = CONFIG.APIURL
-    query = f"?module=transaction&action=getstatus&txhash={txid}&apikey={apikey}"
 
-    url = baseurl + query
-    response = requests.get(url)
+    List_of_txids_db = Mysql.read_txids_from_database()
+    List_of_txids = []
+    for txid_db in List_of_txids_db:
 
-    try:
+            txid_db_each, plan_db, time_db = txid_db
+            List_of_txids.append(txid_db_each)
 
-        if int(response.json()['result']['isError']) == 0 : 
-            return True
-
-        if int(response.json()['result']['isError']) == 1 :
-            return False
-
-        else:
-            return False
-
-    except:
+    if txid in List_of_txids:
         return False
+
+    else:
+
+        apikey = CONFIG.APIKEY
+        baseurl = CONFIG.APIURL
+        query = f"?module=transaction&action=getstatus&txhash={txid}&apikey={apikey}"
+
+        url = baseurl + query
+        response = requests.get(url)
+
+        try:
+
+            if int(response.json()['result']['isError']) == 0 : 
+                Mysql.write_txid_to_database(txid,days)
+                return True
+
+            if int(response.json()['result']['isError']) == 1 :
+                return False
+
+            else:
+                return False
+
+        except:
+            return False
 
 if __name__ == "__main__":
     app.run("0.0.0.0",5550,debug=True)
