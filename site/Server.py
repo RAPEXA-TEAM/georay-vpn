c6d68f5470fa3c2943ae44e9917581dfa7598070
unknown <rapexamrs@gmail.com>
2023-02-03 13:28:51 -0800

    too many changes that work and i dont wana look like stupid fucking programmer to explain all of that in one commit and i am too sorry for myself that i didnt commit them in process i will commit all of changes after this commit

--- site/Server.py
+++ site/Server.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env
 
-from flask import Flask, jsonify, request, render_template, redirect
+from web3 import Web3
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+import random
+import string
+import smtplib
 import Mysql
 import csv
 import hashlib
-import requests
-import names
-import random
+import json
 import CONFIG     #SERVER CONGIG
 
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
 CORS(app)
 
 # config
@@ -18,35 +31,166 @@ app.config.update(
     SECRET_KEY = CONFIG.SECRET_KEY
 )
 
+@app.route("/logout")
+def logout():
+    return redirect(url_for('handle_main_page'))
+
+@app.route('/seller', methods=['POST','GET'])
+@limiter.limit("50 per day")
+def Handle_Seller():
+
+    if request.method == 'POST':
+
+        username = request.json['username']
+        password = request.json['password']
+
+        if Check_Seller(username, password):
+
+            hasha = hashlib.sha256(("{"+str(username)+"}{"+str(password)+"}-georay").encode("utf-8")).hexdigest()
+            ret = {"code" : 200 , "Token" : hasha}
+            return jsonify(ret)
+
+        else:
+            ret = {"code" : 401 , "data" : "Premission denied"}
+            return jsonify(ret)
+
+    return render_template('login.html')
+
+@app.route('/add_sell', methods=['POST', 'GET'])
+@limiter.limit("100 per day")
+def handle_add_sell():
+    if request.method == 'POST':
+        password = generate_random_password()
+        username = request.json["user"]
+        Token_seller = request.json["seller"]
+        token = hashlib.sha256(f"{username}-{password}-{password}-{Token_seller}-{Token_seller}-0-georay".encode('utf-8')).hexdigest()
+
+        Sellers = Read_Sellers()
+        Tokens = []
+
+        for username, password in Sellers.items():  
+            Tokens.append(hashlib.sha256(("{"+str(username)+"}{"+str(password)+"}-georay").encode("utf-8")).hexdigest())
+
+        try :
+            if Check_User(token) and Token_seller in Tokens:
+                if Mysql.write_user_from_seller_to_database(username, password, Token_seller, token):
+                    ret = {"code" : 200, "data" : "user created successfully"}
+                    return jsonify(ret)
+                else :
+                    ret = {"code" : 501, "data" : "error creating user"}
+                    return jsonify(ret)
+            else:
+                ret = {"code" : 404, "data" : "error user already exists"}
+                return jsonify(ret)
+
+        except Exception as error:
+            ret = {"code" : 406, "data" : error}
+            return jsonify(ret)
+
+    ret = {"code" : 500, "data" : "Request not valid"}
+    return jsonify(ret)
+    
+
+@app.route('/sells', methods=['POST','GET'])
+@limiter.limit("100 per day")
+def Handle_Sellers():
+    
+    exec(open('Expiration.py').read())
+
+    if request.method == 'POST':
+
+        days = request.json["days"]
+        token = str(request.json["data"])
+
+        if (Mysql.update_user(token.strip(),days)):
+            ret = {"code" : 200, "data" : f"update {token.strip()} expired days to {days} days!"}
+            return jsonify(ret)
+
+        else:
+            ret = {"code" : 501, "data" : "error connecting to database"}
+            return jsonify(ret)      
+
+    Token = request.args.get('Token')
+    Sellers = Read_Sellers()
+    Tokens = []
+
+    for username, password in Sellers.items():  
+        Tokens.append(hashlib.sha256(("{"+str(username)+"}{"+str(password)+"}-georay").encode("utf-8")).hexdigest())
+
+    if Token in Tokens:
+
+        users = []
+        all_users = Mysql.read_users_for_seller_from_database(Token)
+        user_counter = 0
+        total_sell = 0
+
+        Sellers = Read_Sellers()
+        Seller = {}
+
+        for username_new, password_new in Sellers.items():
+
+            if hashlib.sha256(("{"+str(username_new)+"}{"+str(password_new)+"}-georay").encode("utf-8")).hexdigest() == Token:
+                Seller[username_new] = password_new
+
+            else:
+                continue
+
+        for user in all_users:
+            user_db, password_db, Token_seller, Token_sellerr, days, token_db, verified = user
+            users.append({'username' : user_db, 'password' : password_db, 'days' : days, 'token' : token_db})
+            user_counter += 1
+            total_sell = user_counter * CONFIG.PRICE_ONE_MONTH
+
+        json_data = {"users" : users, "sells" : total_sell, "count" : user_counter, "seller" : Seller}
+        return render_template('seller.html', data = json_data)
+
+    else:
+        return redirect(url_for('Handle_Seller'))
+
+
+@app.route('/makepaymenthash', methods=['POST','GET'])
+def handle_make_payment_hash():
+    
+    if request.method == 'POST':
+
+        user = request.json['user']
+        value = request.json['value']
+        token = request.json['token']
+
+        payment_hash = hashlib.sha256(f"{user}-{value}-{token}-georay".encode('utf-8')).hexdigest()
+
+        ret = {"code" : 200, "payment_hash" : payment_hash}
+        return jsonify(ret)
+
+    ret = {"code" : 500, "data" : "Request not valid"}
+    return jsonify(ret)
+
 @app.route("/payment",methods=["GET","POST"])
 def handle_check_payment():
     if request.method == "POST":
         
         txid = request.json["txid"]
-        days = request.json["days"]
+        user = request.json['user']
+        value = request.json['value']
+        token = request.json['token']
+        days = int(value) / 50000
+        payment_hash = hashlib.sha256(f"{user}-{value}-{token}-georay".encode('utf-8')).hexdigest()
 
-        if Check_Payment(txid,days):
-            username , password = names.get_full_name().split()
-            phone = random.randint(0, 9999999999)
-            email = names.get_full_name().split()[0] + "@gmail.com"
-            answer = names.get_full_name().split()[1]
-            token = hashlib.sha256(f"{username}-{password}-{phone}-{email}-{answer}-georay".encode('utf-8')).hexdigest()
+        if Check_Payment(txid,payment_hash):
+            if Mysql.update_user(token, days) and Mysql.write_txid_to_database(txid, days):
 
-            if Mysql.write_user_to_database(username, password, phone, email, str((int(days)+7)), token):
-
-                ret = {"code" : 200, "data" : {"username" : username , "token" : token}}
+                ret = {"code" : 200, "data" : f"payment successful and payment hash {payment_hash} is valid and user {user} remining days update to {days}"}
                 return jsonify(ret)
 
             else:
-                ret = {"code" : 501, "data" : "'مشکل اتصال به دیتابیس'"}
+                ret = {"code" : 501, "data" : "error connecting to database"}
                 return jsonify(ret)
         
         else:
-            ret = {"code" : 404, "data" : f"txid ({txid}) is not valid"}
+            ret = {"code" : 404, "data" : "txid or payment_hash is not valid"}
             return jsonify(ret)
-        
-    prices = {"1month" : CONFIG.PRICE_ONE_MONTH, "2month" : CONFIG.PRICE_TWO_MONTH, "3month" : CONFIG.PRICE_TRE_MONTH}
-    ret = {"code" : 200, "prices" : prices}
+
+    ret = {"code" : 401, "data" : "request not valid"}
     return jsonify(ret)
 
 @app.route('/d08ec689aef988a788aa6b5f6ed04a0efe57ca919d7d9d863d6322edd47f2d81',methods=["GET", "POST"])
@@ -59,15 +203,22 @@ def handle_admin_page():
             return jsonify(ret)
 
         else:
-            ret = {"code" : 501, "data" : "'مشکل اتصال به دیتابیس'"}
+            ret = {"code" : 501, "data" : "error connecting to database"}
             return jsonify(ret)
             
     all_users = Mysql.read_users_from_database()
     users = []
     for user in all_users:
-        user_db, password_db, phone_number, email, days, token = user
-        users.append({'username' : user_db, 'password' : password_db,'phone' : phone_number, 'email' : email, 'days' : days, 'token' : token})
+        user_db, password_db, phone_number, email, days, token, verified = user
+    
+        if verified != "0":
+            users.append({'username' : user_db, 'password' : password_db,'phone' : phone_number, 'email' : email, 'days' : days, 'token' : token})
+        
+        else:
+            continue
+
     return render_template("admin.html", data = {"users" : users})   
+
 
 @app.route('/register',methods=["GET", "POST"])
 def handle_create_user():
@@ -78,18 +229,19 @@ def handle_create_user():
         phone = request.json["phone"]
         email = request.json["email"]
         answer = request.json["answer"]
-        token = hashlib.sha256(f"{username}-{password}-{rpassword}-{phone}-{email}-{answer}-georay".encode('utf-8')).hexdigest()
+        token = hashlib.sha256(f"{username}-{password}-{password}-{phone}-{email}-{answer}-georay".encode('utf-8')).hexdigest()
 
         try :
             if Check_User(token) and password == rpassword:
-                if Mysql.write_user_to_database(username, password, phone, email, "7", token):
-                    ret = {"code" : 200, "data" : "یوزر با موفقیت ساخته شد"}
+                Send_Registration_Email(email,username,password,phone,answer)
+                if Mysql.write_user_to_database(username, password, phone, email, "7", token, "0"):
+                    ret = {"code" : 200, "data" : "user created successfully but not verified for verification check email that you received"}
                     return jsonify(ret)
                 else :
-                    ret = {"code" : 501, "data" : "'مشکل اتصال به دیتابیس'"}
+                    ret = {"code" : 501, "data" : "error creating user"}
                     return jsonify(ret)
             else:
-                ret = {"code" : 404, "data" : "یوزری با مشخصات ذیل موجود میباشد"}
+                ret = {"code" : 404, "data" : "error user already exists"}
                 return jsonify(ret)
 
         except Exception as error:
@@ -98,30 +250,57 @@ def handle_create_user():
 
     return render_template("register.html")
 
+@app.route('/Authentication', methods=['GET', 'POST'])
+def handle_Authentication_new_user():
+
+    Token = request.args.get('Token')
+    exec(open('Expiration.py').read())
+    
+    if Mysql.update_user_registration(Token):
+    
+        ret = {"code" : 200, "data" : "User verified successfully"}
+        render_template("index.html", data = ret)
+
+    ret = {"code" : 501, "data" : "error verifying user"}
+    return jsonify(ret)
+
 @app.route('/',methods=["GET", "POST"])
 def handle_main_page():
+    
+    # run Expiration.py
+    exec(open('Expiration.py').read())
+
     return render_template("index.html")
 
 @app.route('/login',methods=["GET", "POST"])
 def handle_login_user():
 
+    # run Expiration.py
+    exec(open('Expiration.py').read())
+
     if request.method == 'POST':
         username = request.json["user"]
-        token_in = request.json["token"]
+        passw = request.json["pass"]
 
         list_of_users_dic = {}
         List_Of_Users = Mysql.read_users_from_database()
 
         for user in List_Of_Users:
 
-            user_db, password_db, phone_number, email, days, token = user
-            list_of_users_dic[user_db] = {'password' : password_db,'phone' : phone_number, 'email' : email, 'days' : days, 'token' : token} 
+            user_db, password_db, phone_number, email, days, token, verified = user
+    
+            if verified != "0":
+                list_of_users_dic[user_db] = {'password' : password_db,'phone' : phone_number, 'email' : email, 'days' : days, 'token' : token} 
+
+            else:
+                continue
 
         Servers = Read_servers()
 
-        if (username in list_of_users_dic and token_in == list_of_users_dic[username]["token"]):
-            
-            ret = {"code" : 200, "data" : list_of_users_dic[username], "Servers" : Servers}
+        if (username in list_of_users_dic and passw == list_of_users_dic[username]["password"]):
+
+            prices = {"1month" : CONFIG.PRICE_ONE_MONTH, "2month" : CONFIG.PRICE_TWO_MONTH, "3month" : CONFIG.PRICE_TRE_MONTH}
+            ret = {"code" : 200, "data" : list_of_users_dic[username], "Servers" : Servers , "prices" : prices}
             return jsonify(ret)
         
         else:
@@ -142,12 +321,22 @@ def Read_servers():
     
     return Servers
 
+def Read_Sellers():
+    Sellers = {}
+
+    with open('Sellers.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            Sellers[row[0]] = row[1]
+
+    return Sellers
+
 def Check_User(token):
     
     List_Of_Users = Mysql.read_users_from_database()
     List_of_tokens = []
     for user in List_Of_Users:
-        user_db, password_db, phone_number, email, days, token_db = user 
+        user_db, password_db, phone_number, email, days, token_db, verified = user 
         List_of_tokens.append(token_db)
         
     if token in List_of_tokens:
@@ -156,42 +345,102 @@ def Check_User(token):
     else:
         return True
 
-def Check_Payment(txid,days):
+def Check_Seller(username,password):
+    Sellers = Read_Sellers()
+    for username_for, password_for in Sellers.items():
+        if username_for == username and password_for == password:
+            return True
+    return False
+
+def Check_Payment(txid,payment_hash):
     '''This function check txid is valid or not'''
 
     List_of_txids_db = Mysql.read_txids_from_database()
     List_of_txids = []
+
     for txid_db in List_of_txids_db:
 
             txid_db_each, plan_db, time_db = txid_db
             List_of_txids.append(txid_db_each)
 
     if txid in List_of_txids:
+
         return False
 
     else:
 
-        apikey = CONFIG.APIKEY
-        baseurl = CONFIG.APIURL
-        query = f"?module=transaction&action=getstatus&txhash={txid}&apikey={apikey}"
-
-        url = baseurl + query
-        response = requests.get(url)
-
         try:
 
-            if int(response.json()['result']['isError']) == 0 : 
-                Mysql.write_txid_to_database(txid,days)
+            web3 = Web3(Web3.HTTPProvider(CONFIG.NODE_URL))
+            result = web3.eth.get_transaction(txid)
+            json_result = json.loads(web3.toJSON(result))
+            hash = bytes.fromhex(json_result['input'][2:]).decode('utf-8')
+            
+            if payment_hash == hash and Mysql.write_txid_to_database(txid, days):
                 return True
 
-            if int(response.json()['result']['isError']) == 1 :
-                return False
-
             else:
+        
                 return False
-
+        
         except:
+
             return False
+
+def Send_Registration_Email(email,username,password,phone,answer):
+
+    # create registration token
+    token = hashlib.sha256(f"{username}-{password}-{password}-{phone}-{email}-{answer}-georay".encode('utf-8')).hexdigest()
+
+    # creates Message
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Georay account Registertion"
+    message["From"] = CONFIG.SENDER_EMAIL
+    message["To"] = email
+
+
+    text = f"""\
+    Hi,
+    Check out the link below To register your Georay VPN account:
+    http://127.0.0.1:5550/Authentication?Token={token}
+    """
+
+    html = f"""\
+    <html>
+    <body>
+        <p>Hi,<br>
+        Check out the link below To register your Georay VPN account:</p>
+        <p><a href="http://127.0.0.1:5550/Authentication?Token={token}">Register Now!</a></p>
+    </body>
+    </html>
+    """
+    
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+
+    # creates SMTP session
+    s = smtplib.SMTP(CONFIG.SMTP_SERVER, CONFIG.SMTP_PORT)
+
+    # start TLS for security
+    s.starttls()
+    
+    # Authentication
+    s.login(CONFIG.SENDER_EMAIL, CONFIG.PASSWORD)
+ 
+    # sending the mail
+    s.sendmail(CONFIG.SENDER_EMAIL, email, message.as_string())
+    
+    # terminating the session
+    s.quit()
+    return True
+
+def generate_random_password():
+
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(12))
+    return result_str
 
 if __name__ == "__main__":
     app.run("0.0.0.0",5550,debug=True)
